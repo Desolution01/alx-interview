d51d615eabc4d651fc9c5dbdab88f8373e91c964
Desolution01 <jas3610@gmail.com>
2023-10-18 10:30:47 +0100

    log parsing fixes 3

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,49 @@
 #!/usr/bin/python3
+"""
+A script for parsing HTTP request logs.
+"""
+import re
 import sys
 
-def print_stats(status_counts, total_size):
+def extract_input(input_line):
+    """Extracts sections of a line of an HTTP request log."""
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>[^\]]+)\]',
+        r'\s*"(?P<request>[^"]*)"',
+        r'\s*(?P<status_code>\d+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    log_fmt = '{}-{}-{}-{}-{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match:
+        return (int(resp_match.group('status_code')), int(resp_match.group('file_size')))
+    return None
+
+def print_metrics(total_size, status_codes):
     print("File size: {}".format(total_size))
-    sorted_codes = sorted(status_counts.keys())
-    for code in sorted_codes:
-        if status_counts[code] > 0:
-            print("{}: {}".format(code, status_counts[code]))
+    for code, count in sorted(status_codes.items()):
+        if count:
+            print("{}: {}".format(code, count))
 
-status_counts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
-
-total_size = 0
-line_count = 0
-
-try:
-    for line in sys.stdin:
-        parts = line.split()
-        if len(parts) >= 9:
-            status = int(parts[-2])
-            if status in status_counts:
-                status_counts[status] += 1
-            total_size += int(parts[-1])
+if __name__ == "__main__":
+    total_size = 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    try:
+        line_count = 0
+        while True:
+            line = sys.stdin.readline()
             line_count += 1
+            if not line:
+                break
+            log_data = extract_input(line)
+            if log_data:
+                status_code, file_size = log_data
+                total_size += file_size
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
             if line_count % 10 == 0:
-                print_stats(status_counts, total_size)
-
-except KeyboardInterrupt:
-    pass
-
-print_stats(status_counts, total_size)
+                print_metrics(total_size, status_codes)
+    except KeyboardInterrupt:
+        pass
+    print_metrics(total_size, status_codes)
