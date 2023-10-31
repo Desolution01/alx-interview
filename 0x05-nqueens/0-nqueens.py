32b9cd7136c8567890587612d8aeb63788801941
Desolution01 <jas3610@gmail.com>
2023-10-31 13:14:03 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,49 +1,48 @@
 #!/usr/bin/python3
+
 import sys
 
 def is_safe(board, row, col, n):
-    for i in range(col):
-        if board[row][i] == 1:
+    # Check if no queen attacks this cell in the same column
+    for i in range(row):
+        if board[i][col] == 1:
             return False
+
+    # Check if no queen attacks this cell in the upper-left diagonal
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+
+    # Check if no queen attacks this cell in the upper-right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, n)):
         if board[i][j] == 1:
             return False
+
     return True
-
-def solve_nqueens_util(board, col, n, solutions):
-    if col == n:
-        solution = []
-        for i in range(n):
-            row = [board[i][j] for j in range(n)]
-            solution.append(row)
-        solutions.append(solution)
-        return
-
-    for i in range(n):
-        if is_safe(board, i, col, n):
-            board[i][col] = 1
-            solve_nqueens_util(board, col + 1, n, solutions)
-            board[i][col] = 0
 
 def solve_nqueens(n):
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [[0] * n for _ in range(n)]
+    board = [[0 for _ in range(n)] for _ in range(n)]
     solutions = []
-    solve_nqueens_util(board, 0, n, solutions)
 
-    if not solutions:
-        print("No solution exists.")
-    else:
-        for solution in solutions:
-            for row in solution:
-                print(row)
-            print()
+    def backtrack(row):
+        if row == n:
+            solutions.append([[i, row.index(1)] for i, row in enumerate(board)])
+            return
+
+        for col in range(n):
+            if is_safe(board, row, col, n):
+                board[row][col] = 1
+                backtrack(row + 1)
+                board[row][col] = 0
+
+    backtrack(0)
+    
+    for solution in solutions:
+        print(solution)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -51,9 +50,9 @@ if __name__ == "__main__":
         sys.exit(1)
 
     try:
-        n = int(sys.argv[1])
-        solve_nqueens(n)
+        N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
+    solve_nqueens(N)
